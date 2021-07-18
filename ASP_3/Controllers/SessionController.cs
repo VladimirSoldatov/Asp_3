e0daa419a04a7f1db9da553a9c114652316cb983
Владимир Солдатов <cbb110@rambler.ru>
2021-07-18 13:05:53 +0300

    Second Commit

--- ASP_3/Controllers/SessionController.cs
+++ ASP_3/Controllers/SessionController.cs
@@ -1,21 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASP_3
 {
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
     public class SessionController
     {
         public List<string> sessions { set; get; }
         public SessionController(Film film)
         {
+
             sessions.AddRange(film.Session);
         }
         public void AddSession(string session)
         {
             sessions.Add(session);
             sessions.Sort();
+        }
+        [HttpGet]
+        public string GetTime()
+        {
+            return "В работе";
         }
     }
 }
